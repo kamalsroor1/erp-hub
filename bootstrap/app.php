901633617ae42bb1cc867505cbc1bfb99431f40a
kamalsroor1 <teko.2031@gmail.com>
2026-08-20 01:42:31 +0300

    bootstrap: configure inertia middleware and service providers

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -8,6 +8,7 @@ use Illuminate\Http\Request;
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
         web: __DIR__.'/../routes/web.php',
+        api: __DIR__.'/../routes/api.php',
         commands: __DIR__.'/../routes/console.php',
         health: '/up',
     )
@@ -17,6 +18,7 @@ return Application::configure(basePath: dirname(__DIR__))
 
         $middleware->web(append: [
             \App\Http\Middleware\StoreScope::class,
+            \App\Http\Middleware\HandleInertiaRequests::class,
         ]);
 
         $middleware->alias([
--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -2,6 +2,7 @@
 
 return [
     App\Providers\AppServiceProvider::class,
+    App\Providers\TenancyServiceProvider::class,
     Laravel\Telescope\TelescopeServiceProvider::class,
     App\Providers\TelescopeServiceProvider::class,
 ];
