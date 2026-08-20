b5a35e940b5b514a55d71fbb73a2bc6bfcc69349
kamalsroor1 <teko.2031@gmail.com>
2026-08-20 15:49:03 +0300

    feat(structure): configure backend/package.json and root monorepo scripts

--- bootstrap/providers.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-return [
-    App\Providers\AppServiceProvider::class,
-    App\Providers\TenancyServiceProvider::class,
-    Laravel\Telescope\TelescopeServiceProvider::class,
-    App\Providers\TelescopeServiceProvider::class,
-];
